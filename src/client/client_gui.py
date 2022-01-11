75ea996884c6dcc5d3ff7a0a7285ad5864ffdef1
WWFUG <b07901047@ntu.edu.tw>
2022-01-11 15:55:23 +0800

    update GUI

--- src/client/client_gui.py
+++ src/client/client_gui.py
@@ -1,11 +1,15 @@
 from PyQt5.QtWidgets import QHBoxLayout, QLabel, QSizePolicy, QVBoxLayout
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton
-from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import pyqtSignal, QTimer
+from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QSlider, QStyle
+from PyQt5.QtGui import QPixmap, QIcon, QPalette
+from PyQt5.QtCore import pyqtSignal, QTimer, Qt
+# from PyQt5.QtMultimedia import QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PIL.ImageQt import ImageQt
 
 from client.client import Client
 from utils.video_streaming import VideoStreaming
+
+IMG_DIR = "image/"
 
 class ClientWindow(QMainWindow):
     _update_image_signal = pyqtSignal()
@@ -19,33 +23,44 @@ class ClientWindow(QMainWindow):
             parent=None):
         super(ClientWindow, self).__init__(parent)
         self.video_player = QLabel()
+        #self.video_player = QVideoWidget()
         self.setup_button = QPushButton()
         self.play_button = QPushButton()
-        self.pause_button = QPushButton()
         self.tear_button = QPushButton()
         self.error_label = QLabel()
+
+        p = self.palette()
+        p.setColor(QPalette.Window, Qt.black)
+        self.setPalette(p)
+
+        self.is_playing = False
+        self.slider = QSlider(Qt.Horizontal)
 
         self._media_client = Client(file_name, host_address, host_port, rtp_port)
         self._update_image_signal.connect(self.update_image)
         self._update_image_timer = QTimer()
         self._update_image_timer.timeout.connect(self._update_image_signal.emit)
 
+
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle("Client")
+        self.setWindowTitle("PyQt5 Media Player")
+        self.setGeometry(350, 100, 1024, 500)
+        self.setWindowIcon(QIcon(IMG_DIR+"player.png"))
+
+        # slider
+        self.slider.setRange(0, 0)
 
         self.setup_button.setEnabled(True)
         self.setup_button.setText('Setup')
         self.setup_button.clicked.connect(self.handle_setup)
 
+        # play Btn
         self.play_button.setEnabled(False)
-        self.play_button.setText('Play')
+        self.play_button.setIcon( self.style().standardIcon(QStyle.SP_MediaPlay) )
         self.play_button.clicked.connect(self.handle_play)
 
-        self.pause_button.setEnabled(False)
-        self.pause_button.setText('Pause')
-        self.pause_button.clicked.connect(self.handle_pause)
 
         self.tear_button.setEnabled(False)
         self.tear_button.setText('Teardown')
@@ -55,22 +70,35 @@ class ClientWindow(QMainWindow):
             QSizePolicy.Preferred,
             QSizePolicy.Maximum)
 
-        central_widget = QWidget(self)
+        
+
+        # slider
+        self.slider.setRange(0,0)
+
+        self.label = QLabel()
+        self.label.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+
+        #create hbox layout
+        hboxLayout = QHBoxLayout()
+        hboxLayout.setContentsMargins(0,0,0,0)
+ 
+        #set widgets to the hbox layout
+        hboxLayout.addWidget(self.setup_button)
+        hboxLayout.addWidget(self.play_button)
+        hboxLayout.addWidget(self.slider)
+        hboxLayout.addWidget(self.tear_button)
+
+        #create vbox layout
+        vboxLayout = QVBoxLayout()
+        vboxLayout.addWidget(self.video_player)
+        vboxLayout.addLayout(hboxLayout)
+        vboxLayout.addWidget(self.label)
+
+        central_widget = QVideoWidget(self)
         self.setCentralWidget(central_widget)
 
-        control_layout = QHBoxLayout()
-        control_layout.setContentsMargins(0, 0, 0, 0)
-        control_layout.addWidget(self.setup_button)
-        control_layout.addWidget(self.play_button)
-        control_layout.addWidget(self.pause_button)
-        control_layout.addWidget(self.tear_button)
+        central_widget.setLayout(vboxLayout)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.video_player)
-        layout.addLayout(control_layout)
-        layout.addWidget(self.error_label)
-
-        central_widget.setLayout(layout)
 
     def update_image(self):
         if not self._media_client.is_receiving_rtp:
@@ -89,25 +117,23 @@ class ClientWindow(QMainWindow):
         self._update_image_timer.start(1000//VideoStreaming.FPS)
 
     def handle_play(self):
-        self._media_client.send_play_request()
-        self.play_button.setEnabled(False)
-        self.pause_button.setEnabled(True)
-
-    def handle_pause(self):
-        self._media_client.send_pause_request()
-        self.pause_button.setEnabled(False)
-        self.play_button.setEnabled(True)
+        if not self.is_playing: ## play request
+            self._media_client.send_play_request()
+            self.play_button.setIcon( self.style().standardIcon(QStyle.SP_MediaPause) )
+            self.is_playing = True
+        else:                   ## pause request
+            self._media_client.send_pause_request()
+            self.play_button.setIcon( self.style().standardIcon(QStyle.SP_MediaPlay) )
+            self.is_playing = False
 
     def handle_teardown(self):
         self._media_client.send_teardown_request()
         self.setup_button.setEnabled(True)
         self.play_button.setEnabled(False)
-        self.pause_button.setEnabled(False)
         exit(0)
 
     def handle_error(self):
         self.play_button.setEnabled(False)
-        self.pause_button.setEnabled(False)
         self.tear_button.setEnabled(False)
         self.error_label.setText(f"Error: {self.media_player.errorString()}")
 
