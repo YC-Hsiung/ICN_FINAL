9efc9685f549e1fe0536b2405333c2ce7b6cc7be
fhcwcsy <fhcwcsy@gmail.com>
2022-01-17 11:37:59 +0800

    fix for slightly better interface

--- src/run_client.sh
+++ src/run_client.sh
@@ -1,1 +1,2 @@
+# the last argument can be 'webcam' or 'file'
 python run_client.py ../src_movie.mjpeg 127.0.0.1 8888 7777 webcam
--- src/run_server.py
+++ src/run_server.py
@@ -2,9 +2,10 @@ from server.server import Server
 import sys
 HOST = '127.0.0.1'
 PORT = 8888
+src_type = 'webcam' # 'file' or 'webcam'
 
 if __name__ == "__main__":
     while True:
-        server = Server(HOST, PORT, 'webcam')
+        server = Server(HOST, PORT, src_type)
         server.run()
 
